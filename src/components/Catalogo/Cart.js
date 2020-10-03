3e057132c97bbd2265a6d62d467fe99ee0173daa
Cristian Fernandez <cristianfernandez@gmail.com>
2020-10-02 23:59:08 -0300

    Desafio Carro

--- src/components/Catalogo/Cart.js
+++ src/components/Catalogo/Cart.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Container } from 'react-bootstrap';
 import Crumb from '../Catalogo/Crumb'
+import { CartContext } from '../../context/CartContext';
 const Cart = () => {
+
+    const [cart] = useContext(CartContext);
 
     return (
         <>
@@ -9,6 +12,7 @@ const Cart = () => {
         <Container>
         <div className="py-5 text-center">
                 <h1 className="text-center">Carrito</h1>
+                <span>Items en carrito: {cart.length}</span>
         </div>
         </Container>
         </>
--- src/components/Catalogo/ItemDetail.js
+++ src/components/Catalogo/ItemDetail.js
@@ -1,16 +1,26 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect,  useContext } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import ItemCount from '../../containers/Contador/ItemCount'
 import Crumb from '../Catalogo/Crumb'
 import BtnComprar from './BtnComprar';
-
+import { CartContext } from '../../context/CartContext';
 
 const ItemDetail = ({data}) => {
    
     const [cantidad, setCantidad] = useState(0);
+    const [cart, setCart] = useContext(CartContext);
 
     const onAddCantidadProducto  = (val) => {
         setCantidad(val);
+    }
+
+    useEffect(() => {
+        console.log(cart);
+      }, [cart]);
+
+    const addToCart = () => {
+        const producto = data;
+        setCart(currentCart => [...currentCart, producto])
     }
 
     return (
@@ -26,7 +36,7 @@ const ItemDetail = ({data}) => {
                 <h3>{data.descripcion}</h3>
             </Col>
             <ItemCount initial="0" min="0"  max="10" onAddProducto={onAddCantidadProducto}/>
-            <BtnComprar cantidad={cantidad}/>
+            <BtnComprar cantidad={cantidad} onClick={addToCart}/>
         </Row>
         </Container>
         </div>
